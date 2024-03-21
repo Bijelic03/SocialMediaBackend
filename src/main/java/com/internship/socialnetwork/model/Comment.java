39d8a512c6e6375011b06f4b842e9c78fa5608ac
Dragan Bijelic <bijelic.03.dragan@gmail.com>
2024-03-21 16:00:53 +0100

    fixed comment

--- src/main/java/com/internship/socialnetwork/model/Comment.java
+++ src/main/java/com/internship/socialnetwork/model/Comment.java
@@ -1,6 +1,8 @@
 package com.internship.socialnetwork.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.Id;
@@ -12,11 +14,11 @@ public class Comment {
     @Id
     private Long id;
 
-//    @OneToOne
-//    private Post post;
-//
-//    @ManyToOne
-//    private Comment parentComment;
+    @OneToOne
+    private Post post;
+
+    @ManyToOne
+    private Comment parentComment;
 
     private User author;
 
